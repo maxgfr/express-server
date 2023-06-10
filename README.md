53fa12a6a018fdfa5d9139054fbeed418883cffd
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-06-10 15:26:04 +0200

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # express-server
+
+A simple node express server with a `POST` and `GET` route that returns a JSON object.
+
+## Commands
+
+```bash
+yarn # install dependencies
+yarn dev # start server in watch mode
+yarn start # start server
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  const headers = req.headers;
+  console.log("Headers:", headers);
+  console.log("Data:", data);
+  res.send({ result: "Data received !" });
+});
+
+app.get("/", (_req, res) => {
+  res.json({ result: "Bonjour :)" });
+});
+
+app.listen(80, () => {
+  console.log("Running on port 80");
+});
