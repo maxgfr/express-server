95a221f3cfea126df59a0efa5a4ad32ec8f4ef0c
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-06-12 00:13:48 +0200

    fix: code

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const http = require("node:http");
 
+const HEADERS = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+};
+
 const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "/") {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, HEADERS);
+    res.end(JSON.stringify({ result: "Hello :)" }));
+  } else if (req.method === "POST" && req.url === "/") {
     let data = "";
     req.on("data", chunk => {
       data += chunk;
     });
-
     req.on("end", () => {
       const parsedData = JSON.parse(data);
       const headers = req.headers;
       console.log("Headers:", headers);
       console.log("Data:", parsedData);
-      res.writeHead(200, {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      });
+      res.writeHead(200, HEADERS);
       res.end(JSON.stringify({ result: "Data received !" }));
     });
-  } else if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    });
-    res.end(JSON.stringify({ result: "Bonjour :)" }));
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
+    res.end(JSON.stringify({ error: "404 - not found" }));
   }
 });
 
